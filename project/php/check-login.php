11df20bd38291354159d5834112b653b08e8fbed
GustavoBastos04 <gubasouza2004@gmail.com>
2024-02-17 12:41:01 -0300

    Add Check Login file
    check-login.php is responsible for check if the user and password are correct.

---
+++ project/php/check-login.php
@@ -0,0 +1,52 @@
+<?php require_once('header.php'); ?>
+
+    <?php 
+        $session_start();
+
+        $occupation = $_POST['inputOccupation'];
+        $name = $_POST['inputName'];
+        $password = $_POST['inputPassword'];
+        
+        require_once('database-connection.php');
+        
+        if($occupation == 'inputStudent'){
+            
+
+            $database = "SELECT * FROM  student  WHERE st_login = '".$name."';";
+            $result = $connection->query($database);
+
+            $search = mysqli_fetch_array($result);
+            if($search != null){
+                if($search['st_password'] == $password){
+                    echo '<a href="menu.php">Saudações</a>';
+                }
+                else{
+                    echo '<a href="../index.php">Login não encontrado</a>';
+                }
+            }
+            else{
+                echo 'Login não encontrado';
+            }
+            $connection->close();
+        }
+        if($occupation == 'inputTeacher'){
+            $database = "SELECT * FROM  teacher  WHERE teacher_login = '".$name."';";
+            $result = $connection->query($database);
+
+            $search = mysqli_fetch_array($result);
+            if($search != null){
+                if($search['st_password'] == $password){
+                    echo '<a href="menu.php">Saudações</a>';
+                }
+                else{
+                    echo '<a href="../index.php">Login não encontrado</a>';
+                }
+            }
+            else{
+                echo 'Login não encontrado';
+            }
+            $connection->close();
+        }
+    ?>
+
+<?php require_once('footer.php'); ?>
